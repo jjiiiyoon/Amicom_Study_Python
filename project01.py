5c8d64c7268bfaac38b6febd14c7a9735d7394a0
jjiiiyoon <my1jy@ajou.ac.kr>
2023-03-24 17:32:32 +0900

    [docs] Study_언어_n주차

---
+++ project01.py
@@ -0,0 +1,11 @@
+#문제 1
+number = (int)(random() *29) +1
+print("%d개" %25)
+
+#문제 2
+print("  A\n BC\nDEF")
+
+#문제 3
+age = randint(10, 30)
+name = "kelvin"
+print ("안녕하세요 제 이름은 %c%s 입니다. 제 나이는 %d 입니다. " %(name[0].upper(), name[1:], age))
